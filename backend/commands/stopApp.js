8be56941cd229e64524f838dd85a23fb15a91be3
Mike Vincent <6988839+nonanom@users.noreply.github.com>
2023-09-12 22:10:31 -0700

    Create stopApp.js

---
+++ backend/commands/stopApp.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+
+module.exports = (req, res) => {
+    const appName = req.body.appName;
+    exec(`shell_command_to_stop_app ${appName}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return res.status(500).send('Internal Server Error');
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        res.sendStatus(200);
+    });
+};
